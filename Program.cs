5495be20e7725915e15eaa12608c83ee420745bb
aleqs83 <aleqsavoyan@gmail.com>
2025-12-31 00:04:50 +0200

    added q3 and q4 fixed q2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ using System.Xml.Linq;
 
 namespace AlexAvoyan_3._2_2025
 {
+
     internal class Program
     {
         public static Node<int> FromArrToNode(int[] arr)
@@ -42,49 +44,98 @@ namespace AlexAvoyan_3._2_2025
         }
         static int Width(Node<int> lst, int num)
         {
-            Node<int> p = lst;
-
-            // חיפוש המספר num
-            while (p != null && p.GetValue() != num)
-                p = p.GetNext();
-
-            if (p == null)
-                return -1; // num לא קיים בשרשרת
-
             int count = 1;
-            bool positive = p.GetValue() > 0;
-            p = p.GetNext();
-
-            // ספירת תת-הסדרה הנגדית
-            while (p != null)
+            while (lst.GetValue() != num && lst.GetValue() != -num)
             {
-                if ((positive && p.GetValue() < 0) || (!positive && p.GetValue() > 0))
-                {
-                    count++;
-                    p = p.GetNext();
-                }
-                else
-                    break;
+                lst = lst.GetNext();
             }
-
+            int numToFind = -lst.GetValue();
+            while (lst != null && lst.GetValue() != numToFind)
+            {
+                lst = lst.GetNext();
+                count++;
+            }
+            if (lst == null)
+                return -1;
             return count;
         }
-        public static int Longest(Node<int> lst)
+        static int Longest(Node<int> lst)
         {
-            int max = 0;
-            Node<int> p = lst;
-
-            while (p != null)
+            int max = -1;
+            while (lst != null)
             {
-                int w = Width(lst, p.GetValue());
+                int w = Width(lst, lst.GetValue());
                 if (w > max)
+                {
                     max = w;
-
-                p = p.GetNext();
+                }
+                lst = lst.GetNext();
             }
-
-            return max == 0 ? -1 : max;
+            return max;
         }
+        public static int CountList(Node<int> lst)//o(N) n- list size
+        {
+            int count = 0;
+            Node<int> pos = lst;
+            while (pos != null)
+            {
+                count++;
+                pos = pos.GetNext();
+            }
+            return count;
+        }
+        static Node<int> Move(Node<int> lst, int n)//o(N) n- list size
+        {
+            int count = CountList(lst);
+            Node<int> pos = lst;
+            for (int i = 0; i < count - n - 1; i++)
+            {
+                pos = pos.GetNext();
+            }
+            Node<int> pos2 = pos;
+            while (pos2.GetNext() != null)
+            {
+                pos2 = pos2.GetNext();
+            }
+            Node<int> newHead = pos.GetNext();
+            pos.SetNext(null);
+            pos2.SetNext(lst);
+            return newHead;
+        }
+        static Node<int> BuildDigit(Node<int>lst)
+        {
+            Node<int> pos = lst;
+            Node<int> pos2 = null;
+            int x, num = 0;
+            int p = 1;
+            Node<int> listDigit = null;
+            while(pos != null)
+            {
+                x = pos.GetValue();
+                if(x != -9)
+                {
+                    num = num + x * p;
+                    p = p * 10;
+                }
+                else
+                {
+                    if(listDigit ==null)
+                    {
+                        listDigit = new Node<int>(num);
+                        pos2 = listDigit;
+                    }
+                    else
+                    {
+                        pos2.SetNext(new Node<int>(num));
+                        pos2=pos2.GetNext();
+                    }
+                    num = 0; p = 1;
+                }
+                pos = pos.GetNext();
+            }
+            return listDigit;
+        }
+
         static void Main(string[] args)
         {
             //int[] arr = { 1, 2, 3, 4, 5 };
@@ -92,13 +143,21 @@ namespace AlexAvoyan_3._2_2025
             //Console.WriteLine(IsArranged(lst)); // Output: True
             //int[] arr2 = { 1, 3, 2, 4, 5 };
             //Node<int> lst2 = FromArrToNode(arr2);
-            //Console.WriteLine(IsArranged(lst2)); // Output: False
-            int[] arr3 = { -9, -1, 1, -22 , -10, -2, 9, -10 ,4};
-            Node<int> lst3 = FromArrToNode(arr3);
-            Console.WriteLine(Width(lst3, -1)); // Output: 7
-            int[]arr4 = {-9 ,1, 1, 22, 10, 2, 9};
-            Node<int> lst4 = FromArrToNode(arr4);
-            Console.WriteLine(Longest(lst4)); // Output: 45
+            //Console.WriteLine(IsArranged(lst2)); // Output: False\
+            //int[] arr3 = { -9, -1, 1, 22, 10, -2, 9, -10, 4 };
+            //Node<int> lst3 = FromArrToNode(arr3);
+            //Console.WriteLine($"width of 9 is:" + Width(lst3, 9)); // Output: 7
+            //Console.WriteLine($"width of -1 is:" + Width(lst3, -1)); // Output: 7
+            //Console.WriteLine($"width of 22 is:" + Width(lst3, 22)); // Output: 7
+            //Console.WriteLine("longest: " + Longest(lst3));
+            //int[] arr4 = { 5, 1, 2, 8, 4 };
+            //Node<int> lst4 = FromArrToNode(arr4);
+            //Console.WriteLine("the node before it was moved: " + lst4);
+            //Console.WriteLine("the node after it was moved: " + Move(lst4, 2));
+            int[] arr5 = {2,9,-9,4,-9,3,4,5,-9,7,6,-9};
+            Node<int> lst5 = FromArrToNode(arr5);
+            Console.WriteLine(BuildDigit(lst5));
+            
         }
     }
 }
